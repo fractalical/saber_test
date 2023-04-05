af01dfa87fe7c977492e02bbabf7d6c0bc75d73b
Artur <ArthurZ43@yandex.ru>
2023-04-05 14:08:54 +0300

    added new test

--- tests/test_builds.py
+++ tests/test_builds.py
@@ -5,19 +5,20 @@ from conftests import client
 
 
 @pytest.mark.parametrize(
-    'x_api_token,build_name,status_code',
+    'x_api_token,build,build_name,status_code',
     [
-        ('', 'some_name', 403),
-        ('some_api_token', 'some_name', 403),
-        (settings.X_API_TOKEN.get_secret_value(), '', 400),
-        (settings.X_API_TOKEN.get_secret_value(), 'some_name', 404),
-        (settings.X_API_TOKEN.get_secret_value(), 'write_beautiful', 200)
+        ('', 'build', 'some_name', 403),
+        ('some_api_token', 'build', 'some_name', 403),
+        (settings.X_API_TOKEN.get_secret_value(), 'build', '', 400),
+        (settings.X_API_TOKEN.get_secret_value(), 'build', 'some_name', 404),
+        (settings.X_API_TOKEN.get_secret_value(), 'build', 'write_beautiful', 200),
+        (settings.X_API_TOKEN.get_secret_value(), 'builds', 'write_beautiful', 422)
     ]
 )
-def test_get_tasks(x_api_token, build_name, status_code):
+def test_get_tasks(x_api_token, build, build_name, status_code):
     headers = {'X-API-TOKEN': x_api_token}
     response = client.post('/build/get_tasks', headers=headers, json={
-        'build': build_name
+        build: build_name
     })
 
     assert response.status_code == status_code
